f40772fd466deffe2e0874b4ff8ff68070e59c81
YeyoM <yeyomoreno2003@hotmail.com>
2021-09-04 21:46:26 -0500

    Return to passport session

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(passport.initialize());
-//app.use(passport.session());
+app.use(passport.session());
 app.use(flash());
 app.use(
     express.json({
