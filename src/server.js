2557e66ed7eb55d877897ba09cec65afc830fef4
YeyoM <yeyomoreno2003@hotmail.com>
2021-09-05 12:17:37 -0500

    a

--- src/server.js
+++ src/server.js
@@ -54,7 +54,7 @@ app.use(
 );
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
