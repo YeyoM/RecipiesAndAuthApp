d21b40ccedf51e73d39ba3e6435871913857d69f
YeyoM <yeyomoreno2003@hotmail.com>
2021-09-05 12:44:48 -0500

    logging again

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -298,11 +298,11 @@ usersCtrl.createCheckoutSession = async (req, res) => {
                 },
             ],
             // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-            success_url: `${domainURL}/`,
+            success_url: `${domainURL}/users/signin`,
             cancel_url: `${domainURL}/`,
         })
         req.flash('success_msg', 'Payment recieved successfully, now just confirm email if you have not');
-        console.log(session)
+        console.log(session.url)
         return res.redirect(303, session.url);  
     } catch (e) {
         res.status(400);
